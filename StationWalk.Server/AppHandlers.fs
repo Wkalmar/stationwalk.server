6bed2c00f112b3dddc81aeeef075c8b9d08c92fa
Bohdan Stupak <stupakbogdan@ukr.net>
2018-06-15 23:19:10 +0300

    Migration to girafe and AWS configuration

---
+++ StationWalk.Server/AppHandlers.fs
@@ -0,0 +1,32 @@
+﻿module AppHandlers
+
+open Giraffe
+open System
+open Microsoft.Extensions.Logging
+
+let getRoutes  = 
+    fun next httpContext ->
+    let result = DAL.getAllRoutes >-> DomainMappers.dbRoutesToRoutes 
+    match result() with
+    | Success s -> json s next httpContext
+    | Failure _ -> setStatusCode 500 next httpContext
+    
+
+let getStations =
+    fun next httpContext ->
+    let result = DAL.getAllStations >-> DomainMappers.dbStationsToStations
+    match result() with
+    | Success s -> json s next httpContext
+    | Failure _ -> setStatusCode 500 next httpContext
+    
+let app:HttpHandler =
+    choose [
+        GET >=> choose [
+            route "/routes" >=> getRoutes
+            route "/stations" >=> getStations
+        ]
+    ]
+
+let errorHandler(ex : Exception) (logger : ILogger) =
+    logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse >=> setStatusCode 500 >=> text ex.Message
---
+++ StationWalk.Server/Setup.fs
@@ -0,0 +1,55 @@
+﻿module Setup
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Logging
+open Giraffe
+open System
+open System.IO
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseGiraffeErrorHandler(AppHandlers.errorHandler)
+        .UseStaticFiles()
+        .UseGiraffe(AppHandlers.app)
+
+let configureServices (services : IServiceCollection) =
+   services.AddGiraffe() |> ignore
+
+let configureLogging (builder : ILoggingBuilder) =
+    let filter (l : LogLevel) = l.Equals LogLevel.Error
+    builder.AddFilter(filter).AddConsole().AddDebug() |> ignore
+
+let configureAppConfiguration (_ : WebHostBuilderContext) (builder : IConfigurationBuilder) =
+    builder.AddEnvironmentVariables() |> ignore
+
+type LambdaEntryPoint() =
+    inherit Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction()
+
+    override this.Init(builder : IWebHostBuilder) =
+        let contentRoot = Directory.GetCurrentDirectory()
+        
+        builder
+            .UseContentRoot(contentRoot) 
+            .Configure(Action<IApplicationBuilder> configureApp)
+            .ConfigureServices(configureServices)
+            |> ignore
+
+
+[<EntryPoint>]
+let main _ =
+    let contentRoot = Directory.GetCurrentDirectory()
+    let webRoot     = Path.Combine(contentRoot, "WebRoot")
+    WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(contentRoot)
+        .UseIISIntegration()
+        .UseWebRoot(webRoot)
+        .ConfigureAppConfiguration(Action<WebHostBuilderContext, IConfigurationBuilder> configureAppConfiguration)
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(configureServices)
+        .ConfigureLogging(configureLogging)
+        .Build()
+        .Run()
+    0
