8c443ed772ed6122a9e8f59282f35058bebd668f
Bohdan Stupak <stupakbogdan@ukr.net>
2020-03-01 18:31:52 +0200

    Add control

--- StationWalk.Server/client/src/controllers/submitController.ts
+++ StationWalk.Server/client/src/controllers/submitController.ts
@@ -3,18 +3,34 @@ import { IController } from "./icontroller";
 import { RouteDrawer } from "../business-logic/routeDrawer";
 import { ControllersEngine } from '../controllersEngine';
 import { ApplicationContext } from '../applicationContext';
-import { Station } from '../models/station';
-import { Debounce } from '../utils/debounce';
+import { StartStationControl } from '../controls/startStationControl';
 
 export class SubmitController implements IController {
-    constructor() {}
+    constructor() {
+        this.startStationControl = new StartStationControl()
+        this.controllerTemplate = 
+        `${this.startStationControl.template}
+        <div id="${this.submitModalId}" class="modal">
+            <div id="${this.submitModalFormId}">
+                <div class="modal-content">
+                    <div>
+                        <label for="${this.routeNameInputId}">Name</label>
+                        <input type="text" id="${this.routeNameInputId}" placeholder="Enter route name...">
+                    </div>
+                    <button id="${this.submitButtonId}">Submit</button>
+                </div>
+            </div>
+            <div id="${this.submitSuccessNotificationContainerId}" class="modal-content" style="display: none;">
+                Your route submitted successfully
+                <button id="${this.gotoHomeButtonId}">Go to home page</button>
+            </div>
+        </div>`
+    }
 
     path = "submit";
 
-    private startStationSelectorId: string = "station-start-select";
-    private startStationInputId: string = "station-start-input";
-    private startStationAutoCompleteContainerId: string = "station-start-autocomplete-container";
-
+    private startStationControl: StartStationControl;
+    
     private submitModalId: string = "submit-modal";
     private submitModalFormId : string = "submit-modal-form";
     private routeNameInputId : string = "route-name";
@@ -23,33 +39,8 @@ export class SubmitController implements IController {
     private gotoHomeButtonId : string = "route-submit-goto-home";
 
     private routeToSubmit : Route;
-
-    private controllerTemplate : string =
-    `
-    <div id="${this.startStationSelectorId}" class="modal" style="display: block;">
-        <div class="modal-content">
-            <div>
-                <label for="${this.startStationInputId}">Start station</label>
-                <input type="text" id="${this.startStationInputId}" placeholder="Enter start station...">
-                <div id="${this.startStationAutoCompleteContainerId}"></div>
-            </div>
-        </div>
-    </div>
-    <div id="${this.submitModalId}" class="modal">
-        <div id="${this.submitModalFormId}">
-            <div class="modal-content">
-                <div>
-                    <label for="${this.routeNameInputId}">Name</label>
-                    <input type="text" id="${this.routeNameInputId}" placeholder="Enter route name...">
-                </div>
-                <button id="${this.submitButtonId}">Submit</button>
-            </div>
-        </div>
-        <div id="${this.submitSuccessNotificationContainerId}" class="modal-content" style="display: none;">
-            Your route submitted successfully
-            <button id="${this.gotoHomeButtonId}">Go to home page</button>
-        </div>
-    </div>`
+    private controllerTemplate: string;
+    
 
     private addSubmitFormEventListeners = () => {
         let submitButton = document.getElementById(this.submitButtonId);
@@ -92,91 +83,14 @@ export class SubmitController implements IController {
     private addMapEventListeners = () => {
         ApplicationContext.map.addEventListener('mousemove', this.addHypotheticalPoint);
         ApplicationContext.map.addEventListener('click', this.addPoint);
-    }
-
-    private addStationStartFormEventListeners = () => {
-        const startStationInput = document.getElementById(this.startStationInputId);
-        if (!startStationInput)
-            throw new Error("Invalid markup. Missing start station input");
-        startStationInput.addEventListener('keyup', Debounce.do(this.searchStation, 500, this));
-    }
-
-    private setStartStation = (e: MouseEvent) => {
-        const emitter = e.target as HTMLElement;
-        if (emitter == null) {
-            throw new Error("Invalid markup. Missing start station input");
-        }
-        let lat = Number(emitter.getAttribute("data-lat"));
-        let lng = Number(emitter.getAttribute("data-lng"));
-        const routeDrawer = RouteDrawer.drawer;
-        routeDrawer.addPoint(lat, lng);
-
-        let autoCompleteContainer = document.getElementById(this.startStationAutoCompleteContainerId);
-        if (!autoCompleteContainer) {
-            throw new Error("Invalid markup. Missing start station autocomplete container");
-        }
-
-        autoCompleteContainer.innerHTML = '';
-
-        let modal = document.getElementById(this.startStationSelectorId);
-        if (!modal) {
-            throw new Error("Invalid markup. Missing start station modal");
-        }
-        modal.style.display = 'none';
-    }
-
-    private displayStartStationAutoComplete = (stations: Station[]) => {
-        let autoCompleteContainer = document.getElementById(this.startStationAutoCompleteContainerId);
-        if (!autoCompleteContainer) {
-            throw new Error("Invalid markup. Missing start station autocomplete container");
-        }
-
-        autoCompleteContainer.innerHTML = '';
-
-        let ul = document.createElement("ul");
-        stations.map(s => {
-            let li = document.createElement("li");
-            li.textContent = s.name;
-
-            const latAttribute = document.createAttribute("data-lat");
-            latAttribute.value = s.location.lattitude.toString();
-            li.attributes.setNamedItem(latAttribute);
-
-            const lngAttribute = document.createAttribute("data-lng");
-            lngAttribute.value = s.location.longitude.toString();
-            li.attributes.setNamedItem(lngAttribute);
-
-            li.addEventListener('click', this.setStartStation);
-            ul.appendChild(li);
-        });
-
-        autoCompleteContainer.appendChild(ul);
-    }
-
-    private searchStation = (e: KeyboardEvent) => {
-        var target = e.target as HTMLTextAreaElement;
-        if (!target)
-            throw new Error("Invalid markup. Missing start station input");
-        fetch(`http://localhost:5000/station/${target.value}`)
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();                        
-                } else {
-                    throw new Error();
-                }
-            })
-            .then(this.displayStartStationAutoComplete)
-            .catch((error) => {
-                console.error(error)
-            });
-    }
+    }   
 
     go = () => {
         this.addControllerTemplate();
         this.addMapEventListeners();
         this.addDrawingSubmittedEventListener();
         this.addSubmitFormEventListeners();
-        this.addStationStartFormEventListeners();
+        this.startStationControl.addEventListeners();
     }
     private addControllerTemplate = () => {
         let controllerTemplateContainer = document.getElementById('controller-template-container');
@@ -193,6 +107,7 @@ export class SubmitController implements IController {
             var container = controllerTemplateContainer as HTMLElement;
             container.remove();
         }
+        this.startStationControl.removeTemplate();
     }
 
     private removeSubmitFormEventListeners = () => {
@@ -215,6 +130,7 @@ export class SubmitController implements IController {
     }
 
     clear = () => {
+        this.startStationControl.removeEventListeners();
         this.removeMapEventListeners();
         this.removeSubmitFormEventListeners();
         this.removeDrawingSubmittedeventListener();
