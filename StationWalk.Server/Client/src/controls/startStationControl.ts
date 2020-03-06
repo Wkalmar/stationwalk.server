c84ac4af273e6e447ebc0f59befb7d2a3a5ce5dc
Bohdan Stupak <stupakbogdan@ukr.net>
2020-03-06 10:19:14 +0200

    Fix gitignore

---
+++ StationWalk.Server/Client/src/controls/startStationControl.ts
@@ -0,0 +1,113 @@
+import { Debounce } from '../utils/debounce';
+import { Station } from '../models/station';
+import { RouteDrawer } from "../business-logic/routeDrawer";
+
+export class StartStationControl {
+    private startStationSelectorId: string = "station-start-select";
+    private startStationInputId: string = "station-start-input";
+    private startStationAutoCompleteContainerId: string = "station-start-autocomplete-container";
+
+    public template: string =
+        `
+        <div id="${this.startStationSelectorId}" class="modal" style="display: block;">
+            <div class="modal-content">
+                <div>
+                    <label for="${this.startStationInputId}">Start station</label>
+                    <input type="text" id="${this.startStationInputId}" placeholder="Enter start station...">
+                    <div id="${this.startStationAutoCompleteContainerId}"></div>
+                </div>
+            </div>
+        </div>
+        `
+    public addEventListeners = () => {
+        const startStationInput = document.getElementById(this.startStationInputId);
+        if (!startStationInput)
+            throw new Error("Invalid markup. Missing start station input");
+        startStationInput.addEventListener('keyup', Debounce.do(this.searchStation, 500, this));
+    }
+
+    public removeEventListeners = () => {
+        const startStationInput = document.getElementById(this.startStationInputId);
+        if (!startStationInput)
+            throw new Error("Invalid markup. Missing start station input");
+        startStationInput.removeEventListener('keyup', Debounce.do(this.searchStation, 500, this));
+    }
+
+    public removeTemplate = () => {
+        const templateContainer = document.getElementById(this.startStationSelectorId);
+        if (templateContainer != null) {
+            var container = templateContainer as HTMLElement;
+            container.remove();
+        }
+    } 
+
+    private searchStation = (e: KeyboardEvent) => {
+        var target = e.target as HTMLTextAreaElement;
+        if (!target)
+            throw new Error("Invalid markup. Missing start station input");
+        fetch(`http://localhost:5000/station/${target.value}`)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error();
+                }
+            })
+            .then(this.displayStartStationAutoComplete)
+            .catch((error) => {
+                console.error(error)
+            });
+    }
+
+    private displayStartStationAutoComplete = (stations: Station[]) => {
+        let autoCompleteContainer = document.getElementById(this.startStationAutoCompleteContainerId);
+        if (!autoCompleteContainer) {
+            throw new Error("Invalid markup. Missing start station autocomplete container");
+        }
+
+        autoCompleteContainer.innerHTML = '';
+
+        let ul = document.createElement("ul");
+        stations.map(s => {
+            let li = document.createElement("li");
+            li.textContent = s.name;
+
+            const latAttribute = document.createAttribute("data-lat");
+            latAttribute.value = s.location.lattitude.toString();
+            li.attributes.setNamedItem(latAttribute);
+
+            const lngAttribute = document.createAttribute("data-lng");
+            lngAttribute.value = s.location.longitude.toString();
+            li.attributes.setNamedItem(lngAttribute);
+
+            li.addEventListener('click', this.setStartStation);
+            ul.appendChild(li);
+        });
+
+        autoCompleteContainer.appendChild(ul);
+    }
+
+    private setStartStation = (e: MouseEvent) => {
+        const emitter = e.target as HTMLElement;
+        if (emitter == null) {
+            throw new Error("Invalid markup. Missing start station input");
+        }
+        let lat = Number(emitter.getAttribute("data-lat"));
+        let lng = Number(emitter.getAttribute("data-lng"));
+        const routeDrawer = RouteDrawer.drawer;
+        routeDrawer.addPoint(lat, lng);
+
+        let autoCompleteContainer = document.getElementById(this.startStationAutoCompleteContainerId);
+        if (!autoCompleteContainer) {
+            throw new Error("Invalid markup. Missing start station autocomplete container");
+        }
+
+        autoCompleteContainer.innerHTML = '';
+
+        let modal = document.getElementById(this.startStationSelectorId);
+        if (!modal) {
+            throw new Error("Invalid markup. Missing start station modal");
+        }
+        modal.style.display = 'none';
+    }
+}
