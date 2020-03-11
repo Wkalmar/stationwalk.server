39996ed6d7ac9caa14bd4c667630da3245f80eb6
Bohdan Stupak <stupakbogdan@ukr.net>
2020-03-11 17:20:20 +0200

    Add welcome screen

--- StationWalk.Server/Client/src/applicationContext.ts
+++ StationWalk.Server/Client/src/applicationContext.ts
@@ -1,3 +1,4 @@
 export class ApplicationContext {
-    public static map : L.Map;
+    public static map: L.Map;
+    public static displayWelcomeScreen: boolean = true;
 }
--- StationWalk.Server/client/src/controllers/homeController.ts
+++ StationWalk.Server/client/src/controllers/homeController.ts
@@ -2,41 +2,54 @@ import { IController } from "./icontroller";
 import { Route } from "../models/route";
 import { RouteToCheckPointsMapper } from "../business-logic/routeToCheckpointsMapper";
 import { ApplicationContext } from '../applicationContext';
+import { WelcomeControl } from "../controls/welcomeControl";
 
-    export class HomeController implements IController {
-        constructor() {}
-
-        path = "home";
-
-        private checkPointsCollection: L.Polyline[] = [];
-
-        routesRequestResolver = (routesResponse: Route[]) => {
-            routesResponse.map((route: Route) => {
-                const mapper = new RouteToCheckPointsMapper(route);
-                let checkpoints = mapper.map();
-                checkpoints.addTo(ApplicationContext.map);
-                this.checkPointsCollection.push(checkpoints);
-            })
-        }
-
-        go = () => {
-            fetch('http://localhost:5000/routes')
-            .then((response) => {
-                if (response.ok) {
-                    ApplicationContext.map.setView([50.425, 30.521], 12);
-                    return response.json();
-                } else {
-                    throw new Error();
-                }
-            })
-            .then(this.routesRequestResolver)
-            .catch((error) => {
-                console.error(error)
-            });
-        }
-
-        clear = () => {
-            this.checkPointsCollection.map(p => p.removeFrom(ApplicationContext.map));
-            this.checkPointsCollection = [];
-        }
+export class HomeController extends IController {
+    constructor() {
+        super()
     }
+
+    private welcomeControl: WelcomeControl = new WelcomeControl();
+
+    path = "home";
+
+    template = `${this.welcomeControl.render()}`
+
+    private checkPointsCollection: L.Polyline[] = [];
+
+    routesRequestResolver = (routesResponse: Route[]) => {
+        routesResponse.map((route: Route) => {
+            const mapper = new RouteToCheckPointsMapper(route);
+            let checkpoints = mapper.map();
+            checkpoints.addTo(ApplicationContext.map);
+            this.checkPointsCollection.push(checkpoints);
+        })
+    }
+
+    go = () => {
+        this.addControllerTemplate();
+        this.welcomeControl.addEventListeners();
+        fetch('http://localhost:5000/routes')
+        .then((response) => {
+            if (response.ok) {
+                ApplicationContext.map.setView([50.425, 30.521], 12);
+                return response.json();
+            } else {
+                throw new Error();
+            }
+        })
+        .then(this.routesRequestResolver)
+        .catch((error) => {
+            console.error(error)
+        });
+    }
+
+    clear = () => {
+        this.checkPointsCollection.map(p => p.removeFrom(ApplicationContext.map));
+        this.checkPointsCollection = [];
+    }
+
+    removeControllerTemplate = () => {
+        this.welcomeControl.removeTemplate();
+    }
+}
