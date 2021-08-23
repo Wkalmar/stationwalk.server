02c232ce6f97be4133d984bbb8b55a936ee07b97
Bohdan Stupak <stupakbogdan@ukr.net>
2021-08-23 10:28:57 +0300

    add debug logging

--- StationWalk.Server/Api/Station/StationApi.fs
+++ StationWalk.Server/Api/Station/StationApi.fs
@@ -7,7 +7,10 @@ open System.Text.Json
 let getAll =
     fun next httpContext ->
     task {
+        Log.instance.Debug("Making call to StationApi.getAll")
         let! dbStations = DbAdapter.getAllStations |> Async.StartAsTask
         let result = DbMappers.dbStationsToStations dbStations
-        return! text (JsonSerializer.Serialize(result, Common.serializerOptions)) next httpContext
+        let serialized = JsonSerializer.Serialize(result, Common.serializerOptions)
+        Log.instance.Debug("Call to StationApi.getAll ended. result: {serialized}", serialized)
+        return! text serialized next httpContext
     }
--- StationWalk.Server/Log.fs
+++ StationWalk.Server/Log.fs
@@ -3,17 +3,10 @@
 open Serilog
 open Serilog.Formatting.Json
 
-let private createLogger =
+let instance =
     LoggerConfiguration()
         .MinimumLevel.Debug()
         .WriteTo.LiterateConsole()
         .WriteTo.RollingFile(JsonFormatter(), "log-{Date}.log")
         .CreateLogger()
 
-let Error (msg : string) =
-    use logger = createLogger
-    logger.Error(msg)
-
-let Exception (ex : exn) =
-    use logger = createLogger
-    logger.Error(ex, "")
